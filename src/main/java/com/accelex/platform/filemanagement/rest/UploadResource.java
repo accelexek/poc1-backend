c0f7a5a4edda193a2a6e5d5995ae889d4f9e2fd8
accelexek <hEwKiVETPW3YQkFEJRuxxrGF>
2018-02-04 22:34:05 +0100

    initial commit

---
+++ src/main/java/com/accelex/platform/filemanagement/rest/UploadResource.java
@@ -0,0 +1,11 @@
+package com.accelex.platform.filemanagement.rest;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/files/upload/v1")
+public class UploadResource {
+}
